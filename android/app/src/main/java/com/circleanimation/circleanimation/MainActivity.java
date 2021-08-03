81613b13789e47d4ca51dd7f15036d6756923256
saber aoufi <saber.aoufi.@gmail.com>
2021-08-03 11:57:01 +0100

    circle animation

---
+++ android/app/src/main/java/com/circleanimation/circleanimation/MainActivity.java
@@ -0,0 +1,6 @@
+package com.circleanimation.circleanimation;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+   
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutte'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+
+@override
+void initState()
+{
+  _animationController = AnimationController( vsync: this, duration: Duration(seconds: 1), );
+super.initState();
+}
+double left=0;
+double right=0;
+double top=0;
+bool taped=false;
+
+  @override
+  Widget build(BuildContext context) {
+     _animationController.repeat();
+
+ _animationController.forward();
+    return Container(child: SafeArea(
+
+      child: Scaffold(
+       
+
+body:Container(
+  alignment: Alignment.center,
+child: Text("hello"),
+
+)
+      ),
+    ),);
+  }
+}  
+class Ball extends CustomPainter {
+
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint _paint = new Paint()
+      ..color =  Colors.green
+      ..strokeWidth = 3;
+
+    canvas.drawCircle(Offset(80, size.height-40 ), 30, _paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class AnimatedCustomPainter extends CustomPainter {
+  final _paint = Paint();
+  final Animation<double> _size;
+  final Animation<double> _offset;
+  final Animation<Color> _color;
+
+  AnimatedCustomPainter(Animation<double> animation)
+      : _size = Tween<double>(begin: 80, end: 300).animate(animation),
+        _offset = Tween<double>(begin: 100, end: 0).animate(animation),
+        _color =
+            ColorTween(begin: Colors.white, end: Colors.blue).animate(animation),
+        super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _paint.color = _color.value;
+    canvas.drawCircle(
+      Offset(
+       0, //size.width / 2,
+        size.height  + _offset.value,
+      ),
+      _size.value,
+      _paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
